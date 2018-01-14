ab526006c0d89bfa5d6715cc1edcfb2968e05679
Antonio Ercole De Luca <eracle@posteo.eu>
2018-01-14 16:02:50 +0100

    #36 - modified the code as requested

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as fp:
-    install_requires = fp.read()
 
 setup(
     name='django-rest-assured',
@@ -12,7 +10,7 @@ setup(
     author_email='maggotfish@gmail.com',
     license='BSD',
     packages=find_packages(),
-    install_requires=install_requires,
+    install_requires=["django>=1.6", "djangorestframework>=2.4.3"],
     zip_safe=False,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
